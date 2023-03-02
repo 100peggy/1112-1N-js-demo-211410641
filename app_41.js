a9589bc800e0462ad15df47b2d78b5d571ab223c
100peggy1~ <candy0903575071@gmail.com>
2023-03-02 21:28:13 +0800

    W03-P4: implement prev button and show the sequence in console.log

--- app_41.js
+++ app_41.js
@@ -1,28 +1,36 @@
 // local reviews data
 const reviews = [
+  
   {
     id: 1,
+    name: '陳鄭升jack',
+    job: 'developer',
+    img: 'https://thkjzjmdttdkwotgyiyj.supabase.co/storage/v1/object/public/demo-41/md_1N_img/w03-p4.png?t=2023-03-02T13%3A23%3A02.059Z',
+    text: "I'm jack"
+  },
+  {
+    id: 2,
     name: 'susan smith',
     job: 'web developer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
-    id: 2,
+    id: 3,
     name: 'anna johnson',
     job: 'web designer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
-    id: 3,
+    id: 4,
     name: 'peter jones',
     job: 'intern',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
-    id: 4,
+    id: 5,
     name: 'bill anderson',
     job: 'the boss',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
@@ -68,6 +76,15 @@ nextBtn.addEventListener('click', () => {
   // console.log('currentItem', reviews[currentItem]);
   showReview(currentItem);
 });
+
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length -1
+  }
+
+  showReview(currentItem);
+})
 
 randomBtn.addEventListener('click', () => {
   currentItem = Math.floor(Math.random() * reviews.length);
