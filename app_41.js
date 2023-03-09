efb68b14bc0b3e0b71796725652d1be078b62479
100peggy <candy0903575071@gmail.com>
2023-03-09 22:03:43 +0800

    w04-logs

--- app_41.js
+++ app_41.js
@@ -74,13 +74,29 @@ const displayMenuItems = (menu) => {
         </div>
       </article>`
     });
-
     console.log('displayMenu before join', displayMenu);
     displayMenu = displayMenu.join('');
     console.log('displayMenu after join', displayMenu);
     sectionCenter.innerHTML = displayMenu;
 }
 
+const categories = ['all', 'breakfast', 'lunch', 'dinner', 'shakes'];
+// const categories = ['all', ...new Set(menu.map((item) => item.category))];
+
+
+const displayMenuButtons = (data) => {
+    let displayMenu = data.map( (category) =>{
+        return `
+        <button type="button" class="filter-btn" data-id=${category}>${category}</button>`
+    });
+
+    console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    btnContainer.innerHTML = displayMenu;
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
+    displayMenuButtons(categories);
 });
