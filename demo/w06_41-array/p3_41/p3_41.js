a8717a00b9fe0e064c226d587d542a220928998d
unknown <candy0903575071@gmail.com>
2023-03-25 23:29:59 +0800

    W06-P5: Temperature convert C2F(), F2C() using Web interface

--- demo/w06_41-array/p3_41/p3_41.js
+++ demo/w06_41-array/p3_41/p3_41.js
@@ -10,15 +10,17 @@ function getUserInput(){
 function outputResult(text){
     currentcalculationOutput.textContent = text;
 }
+
 function c(){
     const operand1 = getUserInput();
     const calcText = `${operand1} `*9/5+32;
     const outputText = `${operand1} C = ${calcText} F`;
     outputResult(outputText);
 }
+
 function f(){
     const operand1 = getUserInput();
-    const calcText = ((`{$operand1} `-32)*5/9).toFixed(2);
+    const calcText = ((`${operand1} `-32)*5/9).toFixed(2);
     const outputText = `${operand1} F = ${calcText} C`;
     outputResult(outputText);
 }
