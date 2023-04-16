051220d3929b0cd39c166e1b2439c2c72a93723a
unknown <candy0903575071@gmail.com>
2023-04-16 17:07:20 +0800

    W09-P3: Midprep P1 answer (c)(d)

--- demo/w09_41/p1_bmi_41/p1_41.js
+++ demo/w09_41/p1_bmi_41/p1_41.js
@@ -1,92 +1,86 @@
 // let height = Number(prompt('輸入你的身高，以公分計算')) / 100;
 // let weight = Number(prompt('輸入你的體種，以公斤計算'));
-const bmi_data_41 = [];
 
-function bmiCalc_41(height, weight) {
-    return weight / (height*height);
+function bmiCalc_39(height, weight) {
+    return weight/(height*height);
 }
 
+function bmiCalcReslt_39(height, weight){
+    let bmi = bmiCalc_39(height, weight).toFixed(2);
+    console.log(`For (h/w) = (${height}/${weight}), then BMI = ${bmi}`);
+} // ``可填入字串，${}可將變書寫在內
 
-function bmiCalcResult_41(height, weight){
-    let bmi = bmiCalc_41(height, weight).toFixed(2);
-    console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi}`);
+// bmiCalcReslt_39(1.75, 55);
+// bmiCalcReslt_39(1.75, 70);
+// bmiCalcReslt_39(1.75, 85);
+
+function bmi_normal_low(height){
+    return 18.5*height*height;
 }
 
-//bmiCalcResult_41(1.75, 55);
-//bmiCalcResult_41(1.75, 70);
-//bmiCalcResult_41(1.75, 85);
-
-function bmi_normal_low (height){
-    return 18.5 * height * height;
+function bmi_normal_high(height){
+    return 24*height*height;
 }
 
-function bmi_normal_high (height){
-    return 24 * height * height;
-}
-
-
-function bmiCalcSuggest_41(height, weight) {
-    let bmi = bmiCalc_41(height, weight).toFixed(2);
-    if (bmi < 18.5){
-        let normal_low = bmi_normal_low(height).toFixed(2);
-        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi} 
-        which is ${(normal_low - weight).toFixed(2)}
-        kg lower than normal`);
-    } else if(bmi <=24) {
-        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi} 
-        which is normal.`);
-    } else {
-        let normal_high = bmi_normal_high(height).toFixed(2);
-        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi}
-        which is ${(normal_high - weight).toFixed(2)} kg lower than normal.`)
+function bmiCalcSuggest_39(height, weight) {
+    let bmi = bmiCalc_39(height, weight).toFixed(2);
+    if(bmi < 18.5){
+        let normal_low = bmi_normal_low(height);
+        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi} which is ${(normal_low - weight).toFixed(2)} kg lower than normal.`)
+    }else if(bmi <=24){
+        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi} which is normal.`)
+    }else{
+        let normal_high = bmi_normal_high(height);
+        console.log(`For (h/w) = (${height}/${weight}), the BMI = ${bmi} which is ${(weight - normal_high).toFixed(2)} kg higher than normal.`)
     }
 }
 
-//bmiCalcSuggest_41(1.75, 55);
-//bmiCalcSuggest_41(1.75, 70);
-//bmiCalcSuggest_41(1.75, 85);
+// bmiCalcSuggest_39(1.75, 55);
+// bmiCalcSuggest_39(1.75, 70);
+// bmiCalcSuggest_39(1.75, 85);
 
-const bmi_data1_41 = [
-    {height : 1.75, width : 55},
-    {height : 1.75, width : 70},
-    {height : 1.75, width : 85},
+const bmi_data_39 = [
+    {height: 1.75, weight: 55},
+    {height: 1.75, weight: 70},
+    {height: 1.75, weight: 85},
 ];
 
-bmi_data_41.forEach((item) => {
-    bmiCalcSuggest_41(item.height, item.weight);
-});
+bmi_data_39.forEach(item=>{
+    bmiCalcSuggest_39(item.height, item.weight);
+})
+
 
 let lowerCount = 0;
 let normalCount = 0;
 let higherCount = 0;
 
-const bmi_data2_41 = [
-    {height : 1.75, width : 55},
-    {height : 1.75, width : 70},
-    {height : 1.75, width : 85},
-    {height : 1.75, width : 50},
-    {height : 1.75, width : 75},
-    {height : 1.75, width : 90},
-    {height : 1.75, width : 80},
-    {height : 1.75, width : 65},
-    {height : 1.75, width : 73},
-    {height : 1.75, width : 72},
+const bmi_data2_39 = [
+    {height: 1.75, weight: 55},
+    {height: 1.75, weight: 70},
+    {height: 1.75, weight: 85},
+    {height: 1.75, weight: 50},
+    {height: 1.75, weight: 75},
+    {height: 1.75, weight: 90},
+    {height: 1.75, weight: 80},
+    {height: 1.75, weight: 65},
+    {height: 1.75, weight: 73},
+    {height: 1.75, weight: 72},
 ];
 
-bmi_data_41.forEach((item) => {
-    const bmi = bmiCalc_41(item.height, item.weight);
-    if( bmi < 18.5) {
-        lowerCount++;
-    } else if(bmi <= 24){
-        normalCount++;
-    } else {
-        higherCount++;
+bmi_data2_39.forEach(item=>{
+    const bmi = bmiCalc_39(item.height, item.weight);
+    if( bmi < 18.5){
+        lowerCount ++;
+    }else if (bmi <= 24){
+        normalCount ++;
+    }else{
+        higherCount ++;
     }
-});
+})
 
 console.log(`
-bmi summary:
-    lower: ${lowerCount}
-    normal: ${normalCount}
-    higher: ${higherCount}
-`);
+    bmi summary:
+        lower: ${lowerCount}
+        normal: ${normalCount}
+        higher: ${higherCount}
+`)
