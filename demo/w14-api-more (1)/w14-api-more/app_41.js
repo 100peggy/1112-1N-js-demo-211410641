0457a78dd1ec224e5078bf473d176001f2f83e29
unknown <candy0903575071@gmail.com>
2023-05-18 20:04:54 +0800

    W14-P2: Get data from MealDB, SportDB, AudioDB one by one as in P1

--- demo/w14-api-more (1)/w14-api-more/app_41.js
+++ demo/w14-api-more (1)/w14-api-more/app_41.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', async () => {
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+  const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
 //const options = {
 	//method: 'GET',
 	//headers: {
@@ -12,8 +12,8 @@ btn.addEventListener('click', async () => {
 try {
 	const response = await fetch(url);
 	const data = await response.json();
-	console.log('data', data.drinks);
-  displayItems(data.drinks);
+	console.log('data', data.meals);
+  displayItems(data.meals);
 } catch (error) {
 	console.error(error);
 }
@@ -22,8 +22,8 @@ try {
 const displayItems = (items) => {
   const displayData = items
     .map((item) => {
-      const { strDrink } = item;
-      return `<p>${strDrink}</p>`;
+      const { strMeal } = item;
+      return `<p>${strMeal}</p>`;
     })
     .join('');
   const element = document.createElement('div');
